5d97ce13b9c44023bd26aadc94ab290a9db55407
ShekinahGloria <shekinah.otieno@strathmore.edu>
2025-04-25 23:52:51 +0300

    Added function to calculate the factorial of a number using a loop.

---
+++ factorial_loop.py
@@ -0,0 +1,8 @@
+def factorial_loop(n):
+    result = 1
+    for i in range(1, n + 1):  # Loop from 1 to n
+        result *= i  # Multiply each value of i with result
+    return result
+
+# Example
+print(factorial_loop(6))  # Outputs 720 because 6! 
